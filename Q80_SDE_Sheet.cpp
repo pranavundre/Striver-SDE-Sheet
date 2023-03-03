4ae8ba36d910536d3ef8489f8cfb2bb71d5a9ac4
Pranav Undre <undrepranav@gmail.com>
2023-03-04 01:37:23 +0530

    Valid Parentheses
    https://leetcode.com/problems/valid-parentheses/
    https://www.youtube.com/watch?v=wkDfsKijrZ8&list=PLgUwDviBIf0p4ozDR_kJJkONnb1wdx2Ma&index=74

---
+++ Q80_SDE_Sheet.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+        for(auto i : s)
+            if(i == '(' || i == '[' || i == '{') stk.emplace(i);
+            else{
+                if(stk.empty() || (i!=')' && stk.top() == '(') || (i!='}' && stk.top() == '{') || (i!=']' && stk.top() == '[')) return false;
+                stk.pop();
+            }
+        return stk.empty();
+    }
+};
