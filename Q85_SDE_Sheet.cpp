f9cd305f26e110af308cf34b9d9ccea850654f06
Pranav Undre <undrepranav@gmail.com>
2023-03-20 11:48:21 +0530

    Largest Rectangle in Histogram
    https://leetcode.com/problems/largest-rectangle-in-histogram/
    https://www.youtube.com/watch?v=jC_cWLy7jSI&list=PLgUwDviBIf0p4ozDR_kJJkONnb1wdx2Ma&index=81

---
+++ Q85_SDE_Sheet.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& histo) {
+        stack<int> st;
+        int maxA = 0;
+        int n = histo.size();
+        for(int i = 0; i <= n; i++){
+            while(!st.empty() && (i==n || histo[st.top()]>=histo[i])){
+                int height = histo[st.top()];
+                st.pop();
+                int width;
+                if(st.empty()) width = i;
+                else width = i - st.top() - 1;
+                maxA = max(maxA, height*width);
+            }
+            st.push(i);
+        }
+        return maxA;
+    }
+};
