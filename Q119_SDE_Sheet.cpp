559a76a42916cf4b35c74a66ab8c84c95c07db41
Pranav Undre <undrepranav@gmail.com>
2023-03-25 21:15:03 +0530

    Balanced Binary Tree
    https://leetcode.com/problems/balanced-binary-tree/
    https://www.youtube.com/watch?v=Yt50Jfbd8Po

--- Q119_SDE_Sheet.cpp
+++ Q119_SDE_Sheet.cpp
@@ -9,7 +9,8 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
- 
+
+// TC = O(N^2)
 class Solution {
 public:
     int height(TreeNode* root){
@@ -22,5 +23,23 @@ public:
         if(diff>1) return false;
         if(!(isBalanced(root->right) && isBalanced(root->left))) return false;
         return true;
+    }
+};
+
+
+// TC = O(N)
+class Solution {
+public:
+    int height(TreeNode* root){
+        if(!root) return 0;
+        int l = height(root->left);  
+        if(l==-1) return -1;
+        int r = height(root->right);
+        if(r==-1) return -1;
+        if(abs(l-r)>1) return -1;
+        return max(l, r)+1;
+    }
+    bool isBalanced(TreeNode* root) {
+        return height(root) != -1;
     }
 };
