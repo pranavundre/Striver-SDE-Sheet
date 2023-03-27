1d7a6b5d23cae3516b9d3a0e11ede49875d23a47
Pranav Undre <undrepranav@gmail.com>
2023-03-27 17:28:58 +0530

    Top View of Binary Tree
    https://www.codingninjas.com/codestudio/problems/799401
    https://www.youtube.com/watch?v=Et9OCDNvJ78

---
+++ Q111_SDE_Sheet.cpp
@@ -0,0 +1,21 @@
+class Solution{
+public:
+    vector<int> topView(Node *root){
+        vector<int> ret;
+        if(!root) return ret;
+        map<int, int> mp;
+        queue<pair<Node*, int>> q;
+        q.emplace(make_pair(root, 0));
+        while(!q.empty()){
+            auto p = q.front();
+            q.pop();
+            Node *temp = p.first;
+            int a = p.second;
+            if(mp.find(a) == mp.end()) mp.insert(make_pair(a, temp->data));
+            if(temp->left) q.emplace(make_pair(temp->left, a-1));
+            if(temp->right) q.emplace(make_pair(temp->right, a+1));
+        }
+        for(auto p : mp) ret.emplace_back(p.second);
+        return ret;
+    }
+};
