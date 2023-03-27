6ec08d5045f619eac9951210fd8df8a4148a91bf
Pranav Undre <undrepranav@gmail.com>
2023-03-27 23:01:24 +0530

    Symmetric Tree
    https://leetcode.com/problems/symmetric-tree/
    https://www.youtube.com/watch?v=nKggNAiEpBE

---
+++ Q127_SDE_Sheet.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+ 
+class Solution {
+public:
+    bool symm(TreeNode* a, TreeNode* b){
+        if(!a || !b) return a==b;
+        return (a->val == b->val && symm(a->left, b->right) && symm(a->right, b->left)); 
+    }
+    bool isSymmetric(TreeNode* root) {
+        return symm(root->left, root->right);
+    }
+};
